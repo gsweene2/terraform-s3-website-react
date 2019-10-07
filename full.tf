64f99fe475015b95b4a25a2c2d367ad846e6d58f
gsweene2 <gsweene2@u.rochester.edu>
2019-10-06 20:37:11 -0400

    Fix hosted zone id

--- full.tf
+++ full.tf
@@ -145,8 +145,8 @@ resource "aws_route53_record" "www" {
   type    = "A"
 
   alias {
-    name                   = "${aws_cloudfront_distribution.s3_distribution.domain_name}"
-    zone_id                = "${data.aws_route53_zone.zone.id}"
+    name                   = replace(aws_cloudfront_distribution.s3_distribution.domain_name, "/[.]$/", "")
+    zone_id                = "${aws_cloudfront_distribution.s3_distribution.hosted_zone_id}"
     evaluate_target_health = true
   }
 
